6b387bf51d1a08fe38f04c742f16e0d8e24668ae
Lorenzo Cioni <lore.cioni@gmail.com>
2016-02-01 12:02:36 +0100

    export page button

--- source/export.php
+++ source/export.php
@@ -43,14 +43,13 @@
 			
 			<div id="content-wrapper">
 
-		    <h1>Press the Button to Export Results</h1>
+		    <h1>Export Results</h1>
+		    <p>Press the button below to export results.</p>
 			<a href="#" class="btn btn-default btn-sm" id="export_res"> Export </a>	
 			</div>
 		</div>
 		</div>
-		<footer>
-			&copy; 2016, MICC University of Florence
-		</footer>
+	
 	    <script src="js/jquery-1.11.1.min.js"></script>
 	    <!-- Include all compiled plugins (below), or include individual files as needed -->
 	    <script src="js/bootstrap.min.js"></script>
