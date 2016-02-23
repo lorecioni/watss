d523f666f73bee8b059e091b1dfd737f633885ca
Lorenzo Cioni <lore.cioni@gmail.com>
2016-02-23 11:19:06 +0100

    export page

--- source/export.php
+++ source/export.php
@@ -44,12 +44,160 @@
 			<div id="content-wrapper">
 
 		    <h1>Export Results</h1>
-		    <p>Press the button below to export results.</p>
-			<a href="#" class="btn btn-default btn-sm" id="export_res"> Export </a>	
+		    <p>Export annotations or the whole database in a SQL script.</p>
+		    <div class="export row">
+				  <div class="col-xs-6">
+					    <div id="db-connection-settings" class="settings-panel panel panel-default">
+						  <div class="panel-heading">
+						    <h3 class="panel-title">Annotations</h3>
+						  </div>
+						  <div class="panel-body">
+								<p>Select annotation attributes you want to export</p>
+							    <div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Person
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Frame
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Camera
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Bounding Box
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Bounding Box visible
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Gaze angle face
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Gaze angle body
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Color
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Point of Interest
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Group
+								  </label>
+								</div>				
+							<div class="clearfix"></div>
+							
+							<p>With the "Export all" button you can download all data (frames and annotations).</p>
+							<div class="checkbox">
+								  <label>
+								    <input type="radio" value="" checked>
+								    Export only annotated frames
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="radio" value="">
+								    Export all frames
+								  </label>
+								</div>	
+							<div class="col-sm-offset-6 col-sm-10">
+								<a href="#" class="btn btn-default" id="export_annotations">Export Annotations</a> <a href="#" class="btn btn-default" id="export_all">Export All</a>
+							</div>
+						  </div>
+						</div>				
+				  </div>
+				  
+				  <div class="col-xs-6">
+				  	<div id="user-settings" class="settings-panel panel panel-default">
+						  <div class="panel-heading">
+						    <h3 class="panel-title">Database</h3>
+						  </div>
+						  <div class="panel-body">
+						  <p>Select database table you want to export</p>
+							    <div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Avatars
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Cameras
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Frames
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Groups
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    People
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Poi
+								  </label>
+								</div>
+								<div class="checkbox">
+								  <label>
+								    <input type="checkbox" value="" checked>
+								    Users
+								  </label>
+								</div>
+						  	<div class="col-sm-offset-6 col-sm-10">
+								<a href="#" class="btn btn-default" id="export_database">Export SQL script</a>
+							</div>
+						  </div>
+						</div>
+				  
+				  </div>		    
+		    </div>
 			</div>
 		</div>
 		</div>
-	
+		<footer>
+			&copy; 2016, MICC University of Florence
+		</footer>
 	    <script src="js/jquery-1.11.1.min.js"></script>
 	    <!-- Include all compiled plugins (below), or include individual files as needed -->
 	    <script src="js/bootstrap.min.js"></script>
