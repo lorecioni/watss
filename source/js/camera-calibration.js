f7fc526973a3b7a2e552b9c9ad204ff8f0e771a4
Lorenzo Cioni <lore.cioni@gmail.com>
2016-03-17 18:54:54 +0100

    checks

--- source/js/camera-calibration.js
+++ source/js/camera-calibration.js
@@ -9,21 +9,23 @@ function loadCameraCalibration(){
 		success: function(response){
 			console.log('Loading camera calibration for the selected camera')
 			cameraCalibration["active"] = response.calibration;
-			cameraCalibration["intrinsic"] = math.eval(response.intrinsic);
-			cameraCalibration["rotation"] = math.eval(response.rotation);
-			cameraCalibration["translation"] = math.eval(response.translation);
-			cameraCalibration["param"] = math.eval(response.param);
-			P = math.concat(math.subset(cameraCalibration.rotation, math.index([0, 1, 2],[0,1])), 
-					cameraCalibration.translation, 1)
-			cameraCalibration['Hw'] = math.multiply(cameraCalibration.intrinsic, P);
-			cameraCalibration['HI2W'] = math.inv(cameraCalibration.Hw);
-			cameraCalibration['HW2I'] = math.transpose(math.inv(cameraCalibration.HI2W));
-			console.log(cameraCalibration)
-			
-			cameraCalibration['l'] = computeInfiniteLine();
-			cameraCalibration['v'] = computeV();
-			
-			computeW();
+			if(cameraCalibration.active){
+				cameraCalibration["intrinsic"] = math.eval(response.intrinsic);
+				cameraCalibration["rotation"] = math.eval(response.rotation);
+				cameraCalibration["translation"] = math.eval(response.translation);
+				cameraCalibration["param"] = math.eval(response.param);
+				P = math.concat(math.subset(cameraCalibration.rotation, math.index([0, 1, 2],[0,1])), 
+						cameraCalibration.translation, 1)
+				cameraCalibration['Hw'] = math.multiply(cameraCalibration.intrinsic, P);
+				cameraCalibration['HI2W'] = math.inv(cameraCalibration.Hw);
+				cameraCalibration['HW2I'] = math.transpose(math.inv(cameraCalibration.HI2W));
+				console.log(cameraCalibration)
+				
+				cameraCalibration['l'] = computeInfiniteLine();
+				cameraCalibration['v'] = computeV();
+				
+				computeW();
+			}
 		}
 	});
 }
